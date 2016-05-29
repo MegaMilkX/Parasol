938a648bf934ddfb9be5f5cf2b7d097ea055d96c
megamilkx <gyrocopter3k@gmail.com>
2016-05-29 17:38:24 +0300

    Turn on blending and backface culling (later should be configurable by materials)

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -99,6 +99,10 @@ GFXTarget* GFXInit(HWND hWnd)
     
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
     
     GFXOldWndProc = (WNDPROC)SetWindowLongPtr(hWnd, GWLP_WNDPROC, (LONG_PTR)GFXWndProc);
     if(!GFXOldWndProc)
