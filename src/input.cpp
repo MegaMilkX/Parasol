30574fbe6f8daab9ace08e8591c37199e0e470c0
megamilkx <gyrocopter3k@gmail.com>
2016-06-05 20:07:09 +0300

    Add basic mouse-keyboard stuff

--- src/input.cpp
+++ src/input.cpp
@@ -1,6 +1,8 @@
 #include "input.h"
-
+#include <iostream>
 int InputMouse::x, InputMouse::y;
+int InputMouse::rel_x, InputMouse::rel_y;
+Action* InputMouse::move_action = 0;
 
 #define RID_COUNT 2
 #define RID_MOUSE 0x02
@@ -9,6 +11,15 @@ int InputMouse::x, InputMouse::y;
 HWND targetWindow;
 WNDPROC OldWndProc;
 RAWINPUTDEVICE rid[RID_COUNT];
+
+enum KEY_STATE
+{
+    UP,
+    DOWN,
+    CLICKED
+};
+
+KEY_STATE kbKeyState[256];
 
 bool showCursor = true;
 
@@ -20,7 +31,39 @@ LRESULT CALLBACK InputWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         InputMouse::_upd(LOWORD(lParam), HIWORD(lParam));
         break;
     case WM_INPUT:
-        
+        {
+            size_t sz = 40;
+            static unsigned char lpb[40];
+
+            GetRawInputData((HRAWINPUT)lParam, RID_INPUT, lpb, &sz, sizeof(RAWINPUTHEADER));
+
+            RAWINPUT* raw = (RAWINPUT*)lpb;
+
+            if (raw->header.dwType == RIM_TYPEMOUSE) // RIM_TYPEMOUSE is zero
+            {
+                int xPosRel = raw->data.mouse.lLastX;
+                int yPosRel = raw->data.mouse.lLastY;
+
+                InputMouse::_updRel(xPosRel, yPosRel);
+                InputMouse::_triggerMoveAction();
+            }
+            else if (raw->header.dwType & RIM_TYPEKEYBOARD)
+            {
+                if (raw->data.keyboard.Flags == RI_KEY_MAKE)
+                {
+                    kbKeyState[raw->data.keyboard.VKey] = KEY_STATE::DOWN;
+                }
+                else if (raw->data.keyboard.Flags & RI_KEY_BREAK)
+                {
+                    kbKeyState[raw->data.keyboard.VKey] = KEY_STATE::UP;
+                }
+                /*
+                if(raw->data.keyboard.Message == WM_KEYDOWN)
+                    kbKeyState[raw->data.keyboard.VKey] = KEY_STATE::DOWN;
+                else if (raw->data.keyboard.Message == WM_KEYUP)
+                    kbKeyState[raw->data.keyboard.VKey] = KEY_STATE::UP;*/
+            }
+        }
         break;
     default:
         return CallWindowProc(OldWndProc, hWnd, msg, wParam, lParam);
@@ -79,4 +122,32 @@ void InputShowCursor(bool show)
 {
     showCursor = show;
     ShowCursor(showCursor);
+}
+
+float InputMouse::GetXNorm()
+{
+    RECT rect;
+    GetClientRect(targetWindow, &rect);
+    return (x / (float)(rect.right - rect.left));
+}
+float InputMouse::GetYNorm()
+{
+    RECT rect;
+    GetClientRect(targetWindow, &rect);
+    return (y / (float)(rect.bottom - rect.top));
+}
+
+void InputMouse::SetMoveCallback(Action& act)
+{
+    if (move_action)
+        delete move_action;
+    move_action = act.clone();
+}
+
+bool InputKB::Key(unsigned char key)
+{
+    if (kbKeyState[key] == KEY_STATE::DOWN)
+        return true;
+    else
+        return false;
 }
--- src/input.h
+++ src/input.h
@@ -3,6 +3,8 @@
 
 #include <windows.h>
 
+#include "action.h"
+
 bool InputInit(HWND hWnd);
 void InputCleanup();
 void InputUpdate();
@@ -14,11 +16,28 @@ class InputMouse
 public:
     static int GetX() { return x; }
     static int GetY() { return y; }
+    static float GetXNorm();
+    static float GetYNorm();
+    static int GetXRel() { return rel_x; }
+    static int GetYRel() { return rel_y; }
+
+    static void SetMoveCallback(Action& act);
 
     static void _upd(int x, int y) { InputMouse::x = x; InputMouse::y = y; }
+    static void _updRel(int x, int y) { InputMouse::rel_x = x; InputMouse::rel_y = y; }
+
+    static void _triggerMoveAction() { move_action->Execute(); }
 private:
     InputMouse() {}
     static int x, y;
+    static int rel_x, rel_y;
+    static Action* move_action;
+};
+
+class InputKB
+{
+public:
+    static bool Key(unsigned char key);
 };
 
 #endif
