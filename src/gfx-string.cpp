386536f45984c59acd483e676143b855547c8a80
megamilkx <gyrocopter3k@gmail.com>
2016-06-28 23:34:36 +0300

    Add GFXString

---
+++ src/gfx-string.cpp
@@ -0,0 +1,59 @@
+#include "gfx-string.h"
+
+GFXString::GFXString()
+	: value(""), size(12), align(LEFT), mesh(GFXMesh::Create(GFXMesh::DYNAMIC))
+{
+	font_face->_RegString(this);
+}
+
+GFXString::GFXString(const std::string& str)
+	: value(str), size(12), align(LEFT), mesh(GFXMesh::Create(GFXMesh::DYNAMIC))
+{
+	font_face->_RegString(this);
+	value = str;
+	MakeMesh();
+}
+
+GFXString::~GFXString()
+{
+	font_face->_UnregString(this);
+	mesh.Destroy();
+}
+
+GFXString& GFXString::operator=(const std::string& str)
+{
+	value = str;
+	MakeMesh();
+	return *this;
+}
+
+void GFXString::Size(unsigned char size)
+{
+	this->size = size;
+	MakeMesh();
+}
+
+void GFXString::Font(const std::string& font_name)
+{
+	font_face->_UnregString(this);
+	font_face = Resource<GFXFont>::Get(font_name, BLOCKING);
+	font_face->_RegString(this);
+	MakeMesh();
+}
+
+void GFXString::Align(int align)
+{
+	this->align = align;
+	MakeMesh();
+}
+
+void GFXString::Render()
+{
+	font_face->Bind(this->size);
+	mesh.Render();
+}
+
+void GFXString::MakeMesh()
+{
+	font_face->MakeString(this, value, size, align);
+}
---
+++ src/gfx-string.h
@@ -0,0 +1,45 @@
+#ifndef _GFX_STRING_H_
+#define _GFX_STRING_H_
+
+#include <string>
+#include "resource.h"
+#include "gfxfont.h"
+#include "gfxmesh.h"
+
+class GFXString
+{
+public:
+	enum ALIGN
+	{
+		LEFT = 1,
+		RIGHT = 2,
+		TOP = 4,
+		BOTTOM = 8,
+		HCENTER = LEFT | RIGHT,
+		VCENTER = TOP | BOTTOM
+	};
+
+	GFXString();
+	GFXString(const std::string& str);
+	~GFXString();
+	GFXString& operator=(const std::string& str);
+
+	void Size(unsigned char size);
+	void Font(const std::string& font_name);
+	void Align(int align);
+
+	void Render();
+
+	friend GFXFont;
+private:
+	void MakeMesh();
+
+	std::string value;
+	unsigned char size;
+	int align;
+	ResHdl<GFXFont> font_face;
+	ResHdl<GFXMaterial> material;
+	GFXMesh mesh;
+};
+
+#endif
