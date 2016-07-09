e9852f76c3945a410ae2da7a2d3c75ae04ac1db9
megamilkx <gyrocopter3k@gmail.com>
2016-07-10 02:04:22 +0300

    Add GFXSysPlain

---
+++ src/game/gfx-sys-plain.h
@@ -0,0 +1,13 @@
+#ifndef _GFX_SYS_PLAIN_H_
+#define _GFX_SYS_PLAIN_H_
+
+#include "gfx-scene-system.h"
+
+class GFXSysPlain : public IGFXSceneSystem
+{
+    SCENE_SYS_BODY(GFXSysPlain, IGFXSceneSystem)
+public:
+    
+};
+
+#endif
